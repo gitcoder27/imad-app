9105cf46da1b3dffec9774ceac822e0a0772ad0e
gitcoder27 <ayanmails.saha@gmail.com>
2018-02-22 22:51:19 +0530

    intro to javascript

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
 console.log('Loaded!');
+alert("Javascript is here");
+
+//change the text
+var element = document.getElementById('content-text');
+element.innerHTML = "new paragraph changed by Javascript";
+
+//move the image
+var img = document.getElementById('madi');
+
+var margin = 0;
+function moveRight() {
+	margin = margin + 5;
+	img.style.marginLeft = margin + "px";
+};
+
+img.onclick = function() {
+	var interval = setInterval(moveRight, 50);
+};

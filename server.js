0db6a6764588c7b446e60448e2f54581962aab2d
gitcoder27 <ayanmails.saha@gmail.com>
2018-02-22 22:19:25 +0530

    new

--- server.js
+++ server.js
@@ -5,6 +5,71 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+'article-one' : {
+	title : 'Article one | Ayan Saha',
+	heading : 'Article one',
+	date : 'Feb 5, 2018',
+	content : `
+	 <p>yo, this is testing. This is the first article hosted on article one html page</p>
+	            <p>this is the first article hosted on article one html page</p>
+	            <p>this is the first article hosted on article one html page</p>
+	            <p>this is the first article hosted on article one html page</p>`
+	        },
+'article-two' : {
+		title : 'Article 2 | Ayan Saha',
+	heading : 'Article two',
+	date : 'Feb 29, 2018',
+	content : `
+	 			<p>This is testing. This is the first article hosted on article one html page</p>
+	            <p>this is the first article hosted on article one html page</p>`
+	            
+	},
+
+'article-three' : {
+	title : 'Article 3 by ayan',
+	heading : 'ARTICLE 3',
+	date : '22 feb 2018',
+	content : `
+	<p>hey people</p>
+	<p>this is the last page of my web app!!!!!</p>`
+}
+};
+
+function createTemplate(data) {
+	date = data.date;
+	heading = data.heading;
+	title = data.title;
+	content = data.content;
+	var htmltemplate =
+	`<html>
+    <head>
+        <title>${title}</title>
+       	<link href="ui/style.css" rel="stylesheet">
+    </head>
+    <body>
+    	<div class="container">
+	        <div>
+	        <a href="/">Home</a>
+	        </div>
+	        <div>
+	        <h4>${heading}</h4>
+	        </div>
+	        <div>
+	            <h1>${date}</h1>
+	        </div>
+	        <div>
+	            <h1>Hello everyone.</h1>
+	        </div>
+	        <div>
+	            ${content}
+	        </div>
+	    </div>
+    </body>
+</html>`
+return htmltemplate
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -17,17 +82,12 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName', function (req, res) {
+	var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
-app.get('/article-three', function (req, res) {
-  res.send('article three is served');
-});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
